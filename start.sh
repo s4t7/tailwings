3e3a63f9ae8ab7ff66ffa95a23cd86c079e8bb6d
saahir <33964080+saahirlol@users.noreply.github.com>
2024-04-13 17:51:57 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -20,11 +20,58 @@ if [ ! -S /var/run/tailscale/tailscaled.sock ]; then
     echo "tailscaled.sock does not exist. exit!"
     exit 1
 fi
+### Get actual city names
+
+region_code=${FLY_REGION}
+
+
+# Map of fly.io region codes to lowercase city names without country or province
+declare -A region_map
+region-map["ams"]="amsterdam"
+region-map["arn"]="stockholm"
+region-map["atl"]="atlanta"
+region-map["bog"]="bogotá"
+region-map["bom"]="mumbai"
+region-map["bos"]="boston"
+region-map["cdg"]="paris"
+region-map["den"]="denver"
+region-map["dfw"]="dallas"
+region-map["ewr"]="secaucus"
+region-map["eze"]="ezeiza"
+region-map["fra"]="frankfurt"
+region-map["gdl"]="guadalajara"
+region-map["gig"]="rio-de-janeiro"
+region-map["gru"]="sao-paulo"
+region-map["hkg"]="hong-kong"
+region-map["iad"]="ashburn"
+region-map["jnb"]="johannesburg"
+region-map["lax"]="los-angeles"
+region-map["lhr"]="london"
+region-map["mad"]="madrid"
+region-map["mia"]="miami"
+region-map["nrt"]="tokyo"
+region-map["ord"]="chicago"
+region-map["otp"]="bucharest"
+region-map["phx"]="phoenix"
+region-map["qro"]="querétaro"
+region-map["scl"]="santiago"
+region-map["sea"]="seattle"
+region-map["sin"]="singapore"
+region-map["sjc"]="san-jose"
+region-map["syd"]="sydney"
+region-map["waw"]="warsaw"
+region-map["yul"]="montreal"
+region-map["yyz"]="toronto"
+
+
+city_name=${region_map[${region_code,,}]}
+
+#end of this
 
 until /app/tailscale up \
     --login-server=${HS} \
     --authkey=${TAILSCALE_AUTH_KEY} \
-    --hostname=flyio-${FLY_REGION} \
+    --hostname=${city_name}-flyio \
     --advertise-exit-node \
     --ssh
 do
